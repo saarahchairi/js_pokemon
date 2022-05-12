7d491938c75f7591a9c7b1d008195cf8523b4d53
saarahchairi <sarni555@hotmail.com>
2022-05-12 20:24:45 +0200

    jeu terminé, il ne reste plus qu'à configurer ma boucle pour relancer le jeu tant que personne n'est mort

--- public/js/main.js
+++ public/js/main.js
@@ -65,9 +65,10 @@ class Pikachu extends Pokemon {
         }
     }
 }
+
 let niveauPersoPika = Math.floor(Math.random() * (99 - 1)) + 1;
 
-let pika = new Pikachu("Pikachu", niveauPersoPika, 100, 50, ["eclair"]);
+let pika = new Pikachu("Pikachu", niveauPersoPika, 10, 50, ["eclair"]);
 
 //Création de la classe et du personnage carapuce
 class Carapuce extends Pokemon {
@@ -112,7 +113,7 @@ class Carapuce extends Pokemon {
                     break;
             }
         }
-        this.ecume = (ennemi) => {
+        this.etrainte = (ennemi) => {
             switch (ennemi.type) {
                 case "eclair":
                     ennemi.vie -= 8
@@ -129,7 +130,7 @@ class Carapuce extends Pokemon {
 }
 let niveauPersoCara = Math.floor(Math.random() * (99 - 1)) + 1;
 
-let cara = new Carapuce("Carapuce", niveauPersoCara, 100, 20, ["eau"]);
+let cara = new Carapuce("Carapuce", niveauPersoCara, 10, 20, ["eau"]);
 
 //Création de la classe et du personnage onix
 class Onix extends Pokemon {
@@ -148,7 +149,7 @@ class Onix extends Pokemon {
                     break;
             }
         }
-        this.morsure = (ennemi) => {
+        this.armure = (ennemi) => {
             switch (ennemi.type) {
                 case "eclair":
                     ennemi.vie -= 10
@@ -161,7 +162,7 @@ class Onix extends Pokemon {
                     break;
             }
         }
-        this.vibraqua = (ennemi) => {
+        this.souplesse = (ennemi) => {
             switch (ennemi.type) {
                 case "eclair":
                     ennemi.vie -= 6
@@ -174,7 +175,7 @@ class Onix extends Pokemon {
                     break;
             }
         }
-        this.ecume = (ennemi) => {
+        this.etreinte = (ennemi) => {
             switch (ennemi.type) {
                 case "eclair":
                     ennemi.vie -= 8
@@ -191,7 +192,7 @@ class Onix extends Pokemon {
 }
 let niveauPersoOni = Math.floor(Math.random() * (99 - 1)) + 1;
 
-let oni = new Onix("Onix", niveauPersoOni, 100, 20, ["sol"]);
+let oni = new Onix("Onix", niveauPersoOni, 10, 40, ["sol"]);
 
 //Tableau des joueurs afin de séléctionner un combattant de manière aléatoire
 let combattant = [pika, cara, oni];
@@ -204,21 +205,312 @@ function combat() {
     let poke = null
 
     //Boucle qui permets de choisir le personnage que l'utilisateur aura choisi
-    while(poke === null){
+    while (poke === null) {
         choix = prompt(`Souahites-tu jouer avec ${pika.nom}, ${cara.nom} ou ${oni.nom}?`);
         joueur = choix.charAt(0).toUpperCase() + choix.slice(1);
         for (let i = 0; i < combattant.length; i++) {
             const element = combattant[i];
             for (const [key, value] of Object.entries(element)) {
-                if(key === 'nom' && value === joueur){
+                if (key === 'nom' && value === joueur) {
                     poke = element
-                    console.log(`Tu as décidé de jouer avec ${poke.nom}`);
-                } 
+                    alert(`Tu as décidé de jouer avec ${poke.nom}`);
+                }
             }
         }
     }
 
-    console.log(`Il est l'heure de savoir contre quel Pokemon tu vas combattre ...`);
-    console.log(`combattant ennemie: ${combattantAleatoire.nom}`);
+    alert(`Il est l'heure de savoir contre quel Pokemon tu vas combattre ...`);
+    alert(`combattant ennemie: ${combattantAleatoire.nom}`);
+    alert(`Voyons maintenant lequel de vous lancera la première attaque ...`);
+
+    do {
+        if (poke.vitesse > combattantAleatoire.vitesse) {
+            alert(`${poke.nom}, c'est à toi de lancer une attaque!`);
+            if (poke.nom === "Pikachu") {
+                let choixAttaque = prompt(`Quelle attaque souhaites-tu? (Attention, le nom de l'attaque doit bien être orthographié!): Fatal foudre / queue de fer / charge / quick attack`);
+                switch (choixAttaque) {
+                    case "fatal foudre":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        pika.fatal_foudre(combattantAleatoire);
+                        break;
+                    case "queue de fer":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        pika.queue_de_fer(combattantAleatoire);
+                        break;
+                    case "charge":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        pika.charge(combattantAleatoire);
+                        break;
+                    case "quick attack":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        pika.quick_attack(combattantAleatoire);
+                        break;
+                    default:
+                        break;
+                }
+                alert(`${combattantAleatoire.nom} a encore ${combattantAleatoire.vie} points de vie`)
+            } else if (poke.nom === "Carapuce") {
+                let choixAttaque = prompt(`Quelle attaque souhaites-tu? (Attention, le nom de l'attaque doit bien être orthographié!): pistolet à eau / morsure / vibraqua / écume`);
+                switch (choixAttaque) {
+                    case "pistolet à eau":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        cara.pistolet_a_eau(combattantAleatoire);
+                        break;
+                    case "morsure":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        cara.morsure(combattantAleatoire);
+                        break;
+                    case "vibraqua":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        cara.vibraqua(combattantAleatoire);
+                        break;
+                    case "écume":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        cara.ecume(combattantAleatoire);
+                        break;
+                    default:
+                        break;
+                }
+                alerte(`${combattantAleatoire.nom} a encore ${combattantAleatoire.vie} points de vie`)
+            } else if (poke.nom === "Onix") {
+                let choixAttaque = prompt(`Quelle attaque souhaites-tu? (Attention, le nom de l'attaque doit bien être orthographié!): tourbi sable / armure / souplesse / étreinte`);
+                switch (choixAttaque) {
+                    case "tourbi sable":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        oni.tourbi_sable(combattantAleatoire);
+                        break;
+                    case "armure":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        oni.armure(combattantAleatoire);
+                        break;
+                    case "souplesse":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        oni.souplesse(combattantAleatoire);
+                        break;
+                    case "étreinte":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        oni.etreinte(combattantAleatoire);
+                        break;
+                    default:
+                        break;
+                }
+                alert(`${combattantAleatoire.nom} a encore ${combattantAleatoire.vie} points de vie`)
+            }
+        } else {
+            alert(`${combattantAleatoire.nom}, c'est à toi de lancer une attaque!`);
+            if (combattantAleatoire.nom === "Pikachu") {
+                let tableauAttaquePika = ["fatal", "queue", "charge", "quick"];
+                let attaqueAleatoirePika = tableauAttaquePika[Math.floor(Math.random() * tableauAttaquePika.length)];
+                switch (attaqueAleatoirePika) {
+                    case "fatal":
+                        pika.fatal_foudre(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec fatal foudre!`);
+                        break;
+                    case "queue":
+                        pika.queue_de_fer(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec queue de fer!`);
+                        break;
+                    case "charge":
+                        pika.charge(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec charge!`);
+                        break;
+                    case "quick":
+                        pika.quick_attack(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec quick attack!`);
+                        break;
+                }
+                alert(`${poke.nom}, tu a encore ${poke.vie} points de vie`)
+
+            } else if (combattantAleatoire.nom === "Carapuce") {
+                let tableauAttaqueCara = ["pistolet", "morsure", "vibraqua", "ecume"]
+                let attaqueAleatoireCara = tableauAttaqueCara[Math.floor(Math.random() * tableauAttaqueCara.length)];
+                switch (attaqueAleatoireCara) {
+                    case "pistolet":
+                        cara.pistolet_a_eau(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec pistolet à eau.`);
+                        break;
+                    case "morsure":
+                        cara.morsure(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec morsure.`);
+                        break;
+                    case "vibraqua":
+                        cara.vibraqua(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec vibraqua.`);
+                        break;
+                    case "ecume":
+                        cara.ecume(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec écume.`);
+                        break;
+                }
+                alert(`${poke.nom}, tu a encore ${poke.vie} points de vie`)
+            } else if (combattantAleatoire.nom === "Onix") {
+                let tableauAttaqueOni = ["tourbi", "armure", "souplesse", "etreinte"]
+                let attaqueAleatoireOni = tableauAttaqueOni[Math.floor(Math.random() * tableauAttaqueOni.length)];
+                switch (attaqueAleatoireOni) {
+                    case "tourbi":
+                        oni.tourbi_sable(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec tourbi sable.`);
+                        break;
+                    case "armure":
+                        oni.armure(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec armure.`);
+                        break;
+                    case "souplesse":
+                        oni.souplesse(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec souplesse.`);
+                        break;
+                    case "etreinte":
+                        oni.etreinte(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec étreinte.`);
+                        break;
+                }
+                alert(`${poke.nom}, tu a encore ${poke.vie} points de vie`)
+            }
+        }
+        if (poke.vitesse > combattantAleatoire.vitesse) {
+            alert(`${combattantAleatoire.nom}, c'est à toi de lancer une attaque!`);
+            if (combattantAleatoire.nom === "Pikachu") {
+                let tableauAttaquePika = ["fatal", "queue", "charge", "quick"];
+                let attaqueAleatoirePika = tableauAttaquePika[Math.floor(Math.random() * tableauAttaquePika.length)];
+                switch (attaqueAleatoirePika) {
+                    case "fatal":
+                        pika.fatal_foudre(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec fatal foudre`);
+                        break;
+                    case "queue":
+                        pika.queue_de_fer(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec queue de fer`);
+                        break;
+                    case "charge":
+                        pika.charge(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec charge`);
+                        break;
+                    case "quick":
+                        pika.quick_attack(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec quick attack`);
+                        break;
+                }
+                alert(`${poke.nom}, tu a encore ${poke.vie} points de vie`)
+
+            } else if (combattantAleatoire.nom === "Carapuce") {
+                let tableauAttaqueCara = ["pistolet", "morsure", "vibraqua", "ecume"]
+                let attaqueAleatoireCara = tableauAttaqueCara[Math.floor(Math.random() * tableauAttaqueCara.length)];
+                switch (attaqueAleatoireCara) {
+                    case "pistolet":
+                        cara.pistolet_a_eau(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec pistolet à eau`);
+                        break;
+                    case "morsure":
+                        cara.morsure(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec morsure`);
+                        break;
+                    case "vibraqua":
+                        cara.vibraqua(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec vibraqua`);
+                        break;
+                    case "ecume":
+                        cara.ecume(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec écume`);
+                        break;
+                }
+                alert(`${poke.nom}, tu a encore ${poke.vie} points de vie`)
+            } else if (combattantAleatoire.nom === "Onix") {
+                let tableauAttaqueOni = ["tourbi", "armure", "souplesse", "etreinte"]
+                let attaqueAleatoireOni = tableauAttaqueOni[Math.floor(Math.random() * tableauAttaqueOni.length)];
+                switch (attaqueAleatoireOni) {
+                    case "tourbi":
+                        oni.tourbi_sable(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec tourbi sable`);
+                        break;
+                    case "armure":
+                        oni.armure(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec armure`);
+                        break;
+                    case "souplesse":
+                        oni.souplesse(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec souplesse`);
+                        break;
+                    case "etreinte":
+                        oni.etreinte(poke);
+                        alert(`${combattantAleatoire.nom} vous a attaqué avec étreinte`);
+                        break;
+                }
+                alert(`${poke.nom}, tu a encore ${poke.vie} points de vie`)
+            }
+        } else {
+            alert(`${poke.nom} c'est à ton tour de lancer une attaque!`);
+            if (poke.nom === "Pikachu") {
+                let choixAttaque = prompt(`Quelle attaque souhaites-tu? (Attention, le nom de l'attaque doit bien être orthographié!): Fatal foudre / queue de fer / charge / quick attack`);
+                switch (choixAttaque) {
+                    case "fatal foudre":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        pika.fatal_foudre(combattantAleatoire);
+                        break;
+                    case "queue de fer":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        pika.queue_de_fer(combattantAleatoire);
+                        break;
+                    case "charge":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        pika.charge(combattantAleatoire);
+                        break;
+                    case "quick attack":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        pika.quick_attack(combattantAleatoire);
+                        break;
+                    default:
+                        break;
+                }
+                alert(`${combattantAleatoire.nom}, tu a encore ${combattantAleatoire.vie} points de vie`)
+            } else if (poke.nom === "Carapuce") {
+                let choixAttaque = prompt(`Quelle attaque souhaites-tu? (Attention, le nom de l'attaque doit bien être orthographié!): pistolet à eau / morsure / vibraqua / écume`);
+                switch (choixAttaque) {
+                    case "pistolet à eau":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        cara.pistolet_a_eau(combattantAleatoire);
+                        break;
+                    case "morsure":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        cara.morsure(combattantAleatoire);
+                        break;
+                    case "vibraqua":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        cara.vibraqua(combattantAleatoire);
+                        break;
+                    case "écume":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        cara.ecume(combattantAleatoire);
+                        break;
+                    default:
+                        break;
+                }
+                alert(`${combattantAleatoire.nom} a encore ${combattantAleatoire.vie} points de vie`)
+            } else if (poke.nom === "Onix") {
+                let choixAttaque = prompt(`Quelle attaque souhaites-tu? (Attention, le nom de l'attaque doit bien être orthographié!): tourbi sable / armure / souplesse / étreinte`);
+                switch (choixAttaque) {
+                    case "tourbi sable":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        oni.tourbi_sable(combattantAleatoire);
+                        break;
+                    case "armure":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        oni.armure(combattantAleatoire);
+                        break;
+                    case "souplesse":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        oni.souplesse(combattantAleatoire);
+                        break;
+                    case "étreinte":
+                        alert(`Tu as choisi ${choixAttaque} comme attaque`);
+                        oni.etreinte(combattantAleatoire);
+                        break;
+                    default:
+                        break;
+                }
+                alert(`${combattantAleatoire.nom} a encore ${combattantAleatoire.vie} points de vie`)
+            }
+        }
+    }
+    while (poke.vie <= 0 && combattantAleatoire.vie <= 0);
 }
 combat()
